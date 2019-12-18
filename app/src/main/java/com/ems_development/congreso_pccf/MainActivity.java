b42f61010625d0888f9aa41a99ca51639a49e043
MalenaMoix <moixmalena@gmail.com>
2019-12-18 14:22:07 -0300

    Inicializacion del proyecto

---
+++ app/src/main/java/com/ems_development/congreso_pccf/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ems_development.congreso_pccf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
