852b8e612f25a7497b1115ce7e9c3d521ae3984b
EmanuelT18 <stomas@frsf.utn.edu.ar>
2020-01-14 20:44:54 -0300

    task_interface_Home -Se agrego la actividad que implementa el bottom navigation view -se agregaron todos los fragmentos que conendran sin terminar sus pantallas todavia

---
+++ app/src/main/java/com/ems_development/congreso_pccf/fragments/location/LocationFragment.java
@@ -0,0 +1,20 @@
+package com.ems_development.congreso_pccf.fragments.location;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.ems_development.congreso_pccf.R;
+
+public class LocationFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_location, container, false);
+        return root;
+    }
+}
