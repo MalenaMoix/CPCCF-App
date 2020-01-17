c75d07a8f1eceb5b9464f5efb4016c49001f1af0
EmanuelT18 <stomas@frsf.utn.edu.ar>
2020-01-17 20:31:23 -0300

    Iniciando Fragmento Home -Se agrego card view para contener los datos de la parte superior de la interfaz -Se agrego el logo del congreso Se agrego nombre del congreso -Se comenzo a agregar el car para contar los dias faltantes

--- app/src/main/java/com/ems_development/congreso_pccf/fragments/home/HomeFragment.java
+++ app/src/main/java/com/ems_development/congreso_pccf/fragments/home/HomeFragment.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 
@@ -16,6 +17,12 @@ public class HomeFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+        CardView cardView = root.findViewById(R.id.card_view_home);
+        cardView.setBackgroundResource(R.drawable.card_view_form_home);
+
         return root;
     }
+
+
 }
