6541241e21ca8d3d52cd8cc9f3662c2eb0427e83
EmanuelT18 <stomas@frsf.utn.edu.ar>
2020-01-20 18:31:52 -0300

    Cambios en res -Se cambio la nomenclatura de los colores en ingles -Se cambiaron strings -Se cambio el nombre del fragmento de cronograma de charlas -se reacomodaron todos lo colores cambiados en los layout

--- app/src/main/java/com/ems_development/congreso_pccf/fragments/home/HomeFragment.java
+++ app/src/main/java/com/ems_development/congreso_pccf/fragments/home/HomeFragment.java
@@ -8,14 +8,14 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
 
 
 import com.ems_development.congreso_pccf.R;
 
 public class HomeFragment extends Fragment {
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 
         CardView cardView = root.findViewById(R.id.card_view_home);
