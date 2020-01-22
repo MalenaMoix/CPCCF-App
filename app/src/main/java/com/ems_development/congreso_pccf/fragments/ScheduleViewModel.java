95bbc90047d7be2eec3523ab5d429abf91c7897f
MalenaMoix <moixmalena@gmail.com>
2020-01-22 15:01:09 -0300

    Creacion del modelo del CardView de las charlas

---
+++ app/src/main/java/com/ems_development/congreso_pccf/fragments/ScheduleViewModel.java
@@ -0,0 +1,7 @@
+package com.ems_development.congreso_pccf.fragments;
+
+import androidx.lifecycle.ViewModel;
+
+public class ScheduleViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
