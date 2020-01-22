f71ecd69b825a0916c3f899c7fc860d20f7e9a1d
MalenaMoix <moixmalena@gmail.com>
2020-01-22 18:20:47 -0300

    Agregado de horario y de clases Adapter y ViewHolder

---
+++ app/src/main/java/com/ems_development/congreso_pccf/adapters/ScheduleViewHolder.java
@@ -0,0 +1,25 @@
+package com.ems_development.congreso_pccf.adapters;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.ems_development.congreso_pccf.R;
+
+public class ScheduleViewHolder extends RecyclerView.ViewHolder {
+    TextView chatTimeBeginning, chatTimeFinish, chatLecturer, chatName, chatPlace;
+    ImageView placeHolder;
+
+    public ScheduleViewHolder (@NonNull View base){
+        super(base);
+        chatLecturer = base.findViewById(R.id.editText_lecturer_name);
+        chatName = base.findViewById(R.id.editText_chat_name);
+        chatPlace = base.findViewById(R.id.editText_chat_place);
+        chatTimeBeginning = base.findViewById(R.id.editText_chat_time_beginning);
+        chatTimeFinish = base.findViewById(R.id.editText_chat_time_finish);
+        placeHolder = base.findViewById(R.id.imageView_place_holder);
+    }
+}
