d947e577764d1985f01869720c27f0cefb52e348
EmanuelT18 <38055258+EmanuelT18@users.noreply.github.com>
2020-02-05 20:49:19 -0300

    Feature/task interface home (#2)
    * task_interface_Home
    -Se agrego la actividad que implementa el bottom navigation view
    -se agregaron todos los fragmentos que conendran sin terminar sus pantallas todavia
    
    * Primera version del bottom navigation bar terminada
    -Se agregaron iconos
    -Se agrego un drawable para la forma de la misma
    -se agrego el color solicitado
    -Se acomodo la animacion swift label
    -se quito el paddign en top que tenia el container
    
    * Iniciando Fragmento Home
    -Se agrego card view para contener los datos de la parte superior de la interfaz
    -Se agrego el logo del congreso
    Se agrego nombre del congreso
    -Se comenzo a agregar el car para contar los dias faltantes
    
    * Cambios en res
    -Se cambio la nomenclatura de los colores en ingles
    -Se cambiaron strings
    -Se cambio el nombre del fragmento de cronograma de charlas
    -se reacomodaron todos lo colores cambiados en los layout
    
    * Agregando font style y icono de reloj al fragmento home
    
    * Cambios en interface Home
    -se redondearon solos los bordes de arriba del bottom navigation bar
    -Se cambio de lugar el card del fragmento home
    -Se quito el icono de cantidad de dias faltantes
    
    * Primera version Interface home
    
    * Interface Home terminada - sin iteraccion con logica
    
    * Rebase con rama develop
    
    * Primera version del bottom navigation bar terminada
    -Se agregaron iconos
    -Se agrego un drawable para la forma de la misma
    -se agrego el color solicitado
    -Se acomodo la animacion swift label
    -se quito el paddign en top que tenia el container
    
    * Iniciando Fragmento Home
    -Se agrego card view para contener los datos de la parte superior de la interfaz
    -Se agrego el logo del congreso
    Se agrego nombre del congreso
    -Se comenzo a agregar el car para contar los dias faltantes
    
    * Cambios en res
    -Se cambio la nomenclatura de los colores en ingles
    -Se cambiaron strings
    -Se cambio el nombre del fragmento de cronograma de charlas
    -se reacomodaron todos lo colores cambiados en los layout
    
    * Agregando font style y icono de reloj al fragmento home
    
    * Cambios en interface Home
    -se redondearon solos los bordes de arriba del bottom navigation bar
    -Se cambio de lugar el card del fragmento home
    -Se quito el icono de cantidad de dias faltantes
    
    * Primera version Interface home
    
    * Interface Home terminada - sin iteraccion con logica
    
    * Corrijiendo fuentes de la pantalla Home

---
+++ app/src/main/java/com/ems_development/congreso_pccf/fragments/home/HomeFragment.java
@@ -0,0 +1,28 @@
+package com.ems_development.congreso_pccf.fragments.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+
+
+import com.ems_development.congreso_pccf.R;
+
+public class HomeFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+        CardView cardView = root.findViewById(R.id.card_view_home);
+        cardView.setBackgroundResource(R.drawable.card_view_form_home);
+
+        return root;
+    }
+
+
+}
---
+++ app/src/main/java/com/ems_development/congreso_pccf/fragments/schedule/ScheduleFragment.java
@@ -0,0 +1,21 @@
+package com.ems_development.congreso_pccf.fragments.schedule;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+
+import com.ems_development.congreso_pccf.R;
+
+public class ScheduleFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_schedule, container, false);
+        return root;
+    }
+}
