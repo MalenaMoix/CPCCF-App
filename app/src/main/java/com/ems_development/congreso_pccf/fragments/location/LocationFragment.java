43a4e077a4f7ce4d6c8241950765511076ab0f9c
MalenaMoix <moixmalena@gmail.com>
2020-02-11 11:00:55 -0300

    Cambio de SupportMapFragment por MapView

--- app/src/main/java/com/ems_development/congreso_pccf/fragments/location/LocationFragment.java
+++ app/src/main/java/com/ems_development/congreso_pccf/fragments/location/LocationFragment.java
@@ -1,38 +1,130 @@
 package com.ems_development.congreso_pccf.fragments.location;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 import com.ems_development.congreso_pccf.R;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 
-public class LocationFragment extends Fragment {
+public class LocationFragment extends Fragment implements OnMapReadyCallback {
 
-    //TODO la calle, ciudad y colectivos estan hardcodeados, cuando se tengan los datos, agregarlos como @string
+    //TODO la calle y la ciudad estan hardcodeados, cuando se tengan los datos, agregarlos como @string
 
     private LocationViewModel locationViewModel;
     private View root;
-    //TODO luego eliminar este boton y ver la manera en la cual se accedera al mapa
-    private Button verMapa;
+    private MapView mapView;
+    private GoogleMap map;
+    private static final String MAP_VIEW_BUNDLE_KEY = "MapViewBundleKey";
 
 
     public View onCreateView(@NonNull final LayoutInflater inflater, final ViewGroup container, Bundle savedInstanceState) {
         locationViewModel = ViewModelProviders.of(this).get(LocationViewModel.class);
         root = inflater.inflate(R.layout.fragment_location, container, false);
 
-        verMapa = root.findViewById(R.id.button);
-        verMapa.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                getFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, new MapFragment()).addToBackStack(null).commit();
-            }
-        });
+        mapView = root.findViewById(R.id.map_view);
+
+        Bundle mapViewBundle = null;
+        if (savedInstanceState != null) {
+            mapViewBundle = savedInstanceState.getBundle(MAP_VIEW_BUNDLE_KEY);
+        }
+
+        mapView.onCreate(mapViewBundle);
+        mapView.getMapAsync(this);
 
         return root;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.map = googleMap;
+        updateMap();
+    }
+
+    public void updateMap (){
+
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)){
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 9999);
+            return;
+        }
+        this.map.setMyLocationEnabled(true);
+
+        LocationManager locationManager = (LocationManager)getActivity().getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        Location location = locationManager.getLastKnownLocation(locationManager.getBestProvider(criteria, false));
+        CameraPosition cameraPosition = new CameraPosition.Builder().target(new LatLng(location.getLatitude(), location.getLongitude())).zoom(14.0f).build();
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newCameraPosition(cameraPosition);
+        map.moveCamera(cameraUpdate);
+        map.getUiSettings().setZoomControlsEnabled(true);
+
+        //TODO Modificar el latlng una vez que se disponga de la ubicacion del congreso
+        map.addMarker(new MarkerOptions().position(new LatLng(-31.617375, -60.674830)).title("Congreso Argentino"));
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        Bundle mapViewBundle = outState.getBundle(MAP_VIEW_BUNDLE_KEY);
+        if(mapViewBundle == null){
+            mapViewBundle = new Bundle();
+            outState.putBundle(MAP_VIEW_BUNDLE_KEY, mapViewBundle);
+        }
+        mapView.onSaveInstanceState(mapViewBundle);
+    }
+
+    @Override
+    public void onPause() {
+        mapView.onPause();
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onDestroy() {
+        mapView.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
     }
 }
