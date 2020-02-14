a25fdad6ff93baae91743e32d99550d091d21305
etomas <etomas@hexacta.com>
2020-02-14 18:37:14 -0300

    Agregando drawer navigation a la app

--- app/src/main/java/com/ems_development/congreso_pccf/activities/BottomNavigationViewActivity.java
+++ app/src/main/java/com/ems_development/congreso_pccf/activities/BottomNavigationViewActivity.java
@@ -3,21 +3,42 @@ package com.ems_development.congreso_pccf.activities;
 import android.os.Bundle;
 import com.ems_development.congreso_pccf.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 
 public class BottomNavigationViewActivity extends AppCompatActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.bottom_navigation_view);
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupWithNavController(navView, navController);
+        //Drawer navigation
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationViewDrawer = findViewById(R.id.nav_view);
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home)
+                .setDrawerLayout(drawer)
+                .build();
+        NavController navControllerDrawer = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navControllerDrawer, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationViewDrawer, navControllerDrawer);
+
+        //Bottom navigation
+        BottomNavigationView navigationViewBottom = findViewById(R.id.nav_view);
+        NavController navControllerBottom = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupWithNavController(navigationViewBottom, navControllerBottom);
     }
 }
