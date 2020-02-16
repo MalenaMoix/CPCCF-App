81bbc3537e44c175ab093f6c12fdc1f303115f2d
EmanuelT18 <38055258+EmanuelT18@users.noreply.github.com>
2020-02-16 17:46:04 -0300

    Feature/domain model (#7)
    * Creando clases del modelo de dominio
    
    * Realizando cambios sugeridos
    
    * Agregando lista de charlas a los disertantes

---
+++ app/src/main/java/com/ems_development/congreso_pccf/models/News.java
@@ -0,0 +1,53 @@
+package com.ems_development.congreso_pccf.models;
+
+public class News {
+
+    private String title;
+    private String image;
+    private String content;
+    private User user;
+    private Lecturer lecturer;
+
+    public News() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Lecturer getLecturer() {
+        return lecturer;
+    }
+
+    public void setLecturer(Lecturer lecturer) {
+        this.lecturer = lecturer;
+    }
+}
