f7aea971363134585a9a1e0be3849bb3e8207543
EmanuelT18 <stomas@frsf.utn.edu.ar>
2020-02-16 20:11:40 -0300

    Terminando botones de fragmento News - falta modificar la lista que recibe el adapter

--- app/src/main/java/com/ems_development/congreso_pccf/fragments/news/NewsFragment.java
+++ app/src/main/java/com/ems_development/congreso_pccf/fragments/news/NewsFragment.java
@@ -1,9 +1,11 @@
 package com.ems_development.congreso_pccf.fragments.news;
 
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -22,6 +24,8 @@ public class NewsFragment extends Fragment {
     private RecyclerView.Adapter newsAdapter;
     private RecyclerView.ViewHolder newsViewHolder;
     private RecyclerView.LayoutManager layoutManager;
+    private Button buttonGeneralNews;
+    private Button buttonLecturerNews;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -32,11 +36,39 @@ public class NewsFragment extends Fragment {
         newsRecyclerView.setHasFixedSize(true);
         layoutManager = new LinearLayoutManager(root.getContext());
         newsRecyclerView.setLayoutManager(layoutManager);
+        buttonGeneralNews = root.findViewById(R.id.btn_general_new);
+        buttonLecturerNews = root.findViewById(R.id.btn_lecturer_news);
 
         newsAdapter = new NewsAdapter();
         newsRecyclerView.setAdapter(newsAdapter);
         //TODO los datos del primer card se encuentran hardcodeados
 
+        buttonGeneralNews.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                buttonGeneralNews.setEnabled(false);
+                buttonGeneralNews.setBackgroundResource(R.drawable.button_news_on);
+                buttonGeneralNews.setTextColor(getResources().getColor(R.color.white));
+                buttonLecturerNews.setBackgroundResource(R.drawable.button_lecturer_off);
+                buttonLecturerNews.setTextColor(getResources().getColor(R.color.pink));
+                buttonLecturerNews.setEnabled(true);
+            }
+        });
+
+        buttonLecturerNews.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                buttonLecturerNews.setEnabled(false);
+                buttonLecturerNews.setBackgroundResource(R.drawable.button_lecturer_on);
+                buttonLecturerNews.setTextColor(getResources().getColor(R.color.white));
+                buttonGeneralNews.setBackgroundResource(R.drawable.button_news_off);
+                buttonGeneralNews.setTextColor(getResources().getColor(R.color.pink));
+                buttonGeneralNews.setEnabled(true);
+            }
+        });
+
         return root;
     }
+
+
 }
