b72e7ed815fc35e22c396aac69dcf7432d51f759
EmanuelT18 <38055258+EmanuelT18@users.noreply.github.com>
2020-02-19 22:28:53 -0300

    Feature/interfaz visualizar noticias (#9)
    * task_interface_Home
    -Se agrego la actividad que implementa el bottom navigation view
    -se agregaron todos los fragmentos que conendran sin terminar sus pantallas todavia
    
    * Primera version del bottom navigation bar terminada
    -Se agregaron iconos
    -Se agrego un drawable para la forma de la misma
    -se agrego el color solicitado
    -Se acomodo la animacion swift label
    -se quito el paddign en top que tenia el container
    
    * Iniciando Fragmento Home
    -Se agrego card view para contener los datos de la parte superior de la interfaz
    -Se agrego el logo del congreso
    Se agrego nombre del congreso
    -Se comenzo a agregar el car para contar los dias faltantes
    
    * Cambios en res
    -Se cambio la nomenclatura de los colores en ingles
    -Se cambiaron strings
    -Se cambio el nombre del fragmento de cronograma de charlas
    -se reacomodaron todos lo colores cambiados en los layout
    
    * Agregando font style y icono de reloj al fragmento home
    
    * Cambios en interface Home
    -se redondearon solos los bordes de arriba del bottom navigation bar
    -Se cambio de lugar el card del fragmento home
    -Se quito el icono de cantidad de dias faltantes
    
    * Primera version Interface home
    
    * Interface Home terminada - sin iteraccion con logica
    
    * Rebase con rama develop
    
    * Primera version del bottom navigation bar terminada
    -Se agregaron iconos
    -Se agrego un drawable para la forma de la misma
    -se agrego el color solicitado
    -Se acomodo la animacion swift label
    -se quito el paddign en top que tenia el container
    
    * Iniciando Fragmento Home
    -Se agrego card view para contener los datos de la parte superior de la interfaz
    -Se agrego el logo del congreso
    Se agrego nombre del congreso
    -Se comenzo a agregar el car para contar los dias faltantes
    
    * Cambios en res
    -Se cambio la nomenclatura de los colores en ingles
    -Se cambiaron strings
    -Se cambio el nombre del fragmento de cronograma de charlas
    -se reacomodaron todos lo colores cambiados en los layout
    
    * Agregando font style y icono de reloj al fragmento home
    
    * Cambios en interface Home
    -se redondearon solos los bordes de arriba del bottom navigation bar
    -Se cambio de lugar el card del fragmento home
    -Se quito el icono de cantidad de dias faltantes
    
    * Primera version Interface home
    
    * Interface Home terminada - sin iteraccion con logica
    
    * Corrijiendo fuentes de la pantalla Home
    
    * agregando fragmento para la vista de noticias
    
    * agregando botones para discriminar entre noticias generales y de disertantes
    
    * Terminando botones de fragmento News - falta modificar la lista que recibe el adapter
    
    * Agregando card para disertantes
    
    * Cambiando tamaño y estilos de texto
    
    * Arreglando margenes
    
    * arreglando tamaño de  letras y colores
    
    * Cambio de tamaño de latra

---
+++ app/src/main/java/com/ems_development/congreso_pccf/adapters/news/NewsViewHolder.java
@@ -0,0 +1,23 @@
+package com.ems_development.congreso_pccf.adapters.news;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.ems_development.congreso_pccf.R;
+
+
+public class NewsViewHolder extends RecyclerView.ViewHolder {
+    public TextView txtTitle, txtContent;
+    public ImageView imageNews;
+
+    public NewsViewHolder(@NonNull View base){
+        super(base);
+        txtTitle = base.findViewById(R.id.txt_title);
+        txtContent = base.findViewById(R.id.txt_content);
+        imageNews = base.findViewById(R.id.image_view_new);
+    }
+}
---
+++ app/src/main/java/com/ems_development/congreso_pccf/adapters/schedule/ScheduleViewHolder.java
@@ -0,0 +1,24 @@
+package com.ems_development.congreso_pccf.adapters.schedule;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.ems_development.congreso_pccf.R;
+
+
+public class ScheduleViewHolder extends RecyclerView.ViewHolder {
+    public TextView chatTimeBeginning, chatTimeFinish, chatLecturer, chatName, chatPlace;
+    public ImageView placeHolder;
+
+    public ScheduleViewHolder (@NonNull View base){
+        super(base);
+        chatLecturer = base.findViewById(R.id.editText_lecturer);
+        chatName = base.findViewById(R.id.editText_chat_name);
+        chatPlace = base.findViewById(R.id.editText_chat_place);
+        chatTimeBeginning = base.findViewById(R.id.editText_chat_time_beginning);
+        chatTimeFinish = base.findViewById(R.id.editText_chat_time_finish);
+        placeHolder = base.findViewById(R.id.imageView_place_holder);
+    }
+}
