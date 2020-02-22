4c4a342cfa462c8f0da7143fb08018d85b818bd3
EmanuelT18 <stomas@frsf.utn.edu.ar>
2020-02-22 11:27:10 -0300

    Se agrego pantalla para crear un chat

---
+++ app/src/main/java/com/ems_development/congreso_pccf/fragments/schedule/CreateScheduleViewModel.java
@@ -0,0 +1,17 @@
+package com.ems_development.congreso_pccf.fragments.schedule;
+
+import androidx.lifecycle.ViewModel;
+
+import com.ems_development.congreso_pccf.models.Chat;
+import com.ems_development.congreso_pccf.models.News;
+import com.ems_development.congreso_pccf.models.User;
+
+public class CreateScheduleViewModel extends ViewModel {
+
+    private Chat schedule;
+
+    public void createSchedule(String title, String content, User user) {
+
+    }
+    // TODO: realizar la carga de los datos una vez que se disponga de ellos
+}
